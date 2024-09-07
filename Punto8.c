65145a95646624fa48f46eec1b30b2fe30e1f6a5
Santiago <stdelgado2004@gmail.com>
2024-09-06 23:30:49 -0300

    finalizado punto 8

--- Punto8.c
+++ Punto8.c
@@ -10,9 +10,41 @@ g) en el cuarto cuadrante
 */
 #include <stdio.h>
 
-int main(){
-  
-  
-  
-  return 0;
+int main() {
+    int N, x, y;
+    int origen = 0, eje_x = 0, eje_y = 0;
+    int primer_cuadrante = 0, segundo_cuadrante = 0, tercer_cuadrante = 0, cuarto_cuadrante = 0;
+    printf("Ingresa la cantidad de puntos: ");
+    scanf("%d", &N);
+    
+    for (int i = 1; i <= N; i++) {
+        printf("Ingresa las coordenadas del punto %d (x y): ", i);
+        scanf("%d %d", &x, &y);
+
+        if (x == 0 && y == 0) {
+            origen++;
+        } else if (x == 0) {
+            eje_y++;
+        } else if (y == 0) {
+            eje_x++;
+        } else if (x > 0 && y > 0) {
+            primer_cuadrante++;
+        } else if (x < 0 && y > 0) {
+            segundo_cuadrante++;
+        } else if (x < 0 && y < 0) {
+            tercer_cuadrante++;
+        } else if (x > 0 && y < 0) {
+            cuarto_cuadrante++;
+        }
+    }
+
+    printf("Puntos en el origen: %d\n", origen);
+    printf("Puntos en el eje X: %d\n", eje_x);
+    printf("Puntos en el eje Y: %d\n", eje_y);
+    printf("Puntos en el primer cuadrante: %d\n", primer_cuadrante);
+    printf("Puntos en el segundo cuadrante: %d\n", segundo_cuadrante);
+    printf("Puntos en el tercer cuadrante: %d\n", tercer_cuadrante);
+    printf("Puntos en el cuarto cuadrante: %d\n", cuarto_cuadrante);
+
+    return 0;
 }
