6432355627e798010ae703f6f7eb295d68660cfb
Santiago <stdelgado2004@gmail.com>
2024-09-08 20:58:40 -0300

    finalizado punto 22

--- Punto22.c
+++ Punto22.c
@@ -5,3 +5,30 @@ hasta terminar el mes de 30 días". Calcular:
 a) las cantidades entregadas por el rico y por el pobre,
 b) a partir de qué día el rico le dio más dinero al pobre, si es que esto ocurrió.
 */
+
+#include <stdio.h>
+int main() {
+    int pobre = 0;          
+    int rico = 0;           
+    int pagoPobre = 100;    
+    int pagoRico = 1;       
+    int dia_supera = 0;
+    for (int dia = 1; dia <= 30; dia++) {
+        pobre += pagoPobre;
+        rico += pagoRico;
+        if (rico > pobre && dia_supera == 0) {
+            dia_supera = dia; 
+        }
+        printf("Día %d: Pobre ha pagado $%d, Rico ha pagado $%d\n", dia, pobre, rico);
+        pagoRico *= 2;
+    }
+    printf("Total entregado por el pobre: $%d\n", pobre);
+    printf("Total entregado por el rico: $%d\n", rico);
+    if (dia_supera != -1) {
+        printf("El rico superó al pobre por primera vez en el día %d.\n", dia_supera);
+    } else {
+        printf("El rico nunca superó al pobre.\n");
+    }
+
+    return 0;
+}
